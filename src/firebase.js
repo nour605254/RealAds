a7b8cd80cd2c602914f51ac206ae9ebd4532360f
venomdu237 <49134830+venomdu237@users.noreply.github.com>
2021-02-15 23:00:30 +0100

    maj

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,22 @@ import firebase from "firebase/app"
 import "firebase/auth"
 
 const app = firebase.initializeApp({
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
-    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+    //apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    apiKey: "AIzaSyCZtX_qQb7VpaYNVT373T6ElAd53WU4ZVE",
+    //authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    authDomain: "rpitest - c0e23.firebaseapp.com",
+    //databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    databaseURL: "https://rpitest-c0e23-default-rtdb.firebaseio.com",
+    //projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    projectId: "rpitest - c0e23",
+    //storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    storageBucket: "rpitest - c0e23.appspot.com",
+    //messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    messagingSenderId: "1588447735",
+    //appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    appId: "1: 1588447735: web: e34753d244693df0fbc6f2",
+    //measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+    measurementId: "G - VBT7DTCRTF"
 })
 
 export const auth = app.auth()
