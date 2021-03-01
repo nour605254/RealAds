35214d79db64a2ac7dc5ede74e5b9766dcff685e
venomdu237 <49134830+venomdu237@users.noreply.github.com>
2021-03-02 00:14:21 +0100

    Update Register.js

--- src/components/sections/register/Register.js
+++ src/components/sections/register/Register.js
@@ -12,7 +12,7 @@ import { isValidPhoneNumber } from 'react-phone-number-input'
 import app from '../../../firebase'
 
 export default function Register() {
-
+        const { currentUser } = useAuth()
         const emailRef = useRef()
         const nameRef = useRef()
         const passRef = useRef()
@@ -45,7 +45,7 @@ export default function Register() {
 
                             //updatePhone(phone)
                             const db = app.firestore()
-                            db.collection('Users').add({
+                            db.collection('Users').doc(currentUser.uid).set({
                                 Name: nameRef.current.value,
                                 Phone: phone,
                                 Role: 'CUSTOMER',
